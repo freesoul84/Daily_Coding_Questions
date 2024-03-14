411f8001244bfbdc802689b72c286a6a06872d69
Alkesha Baikar <baikaralkesha@gmail.com>
2024-03-14 10:15:58 +0530

    Update valid_palindrome.py

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -2,6 +2,7 @@
 # ascii of character a to z is : 97 to 122
 # leetcode problem : https://leetcode.com/problems/valid-palindrome/
 
+# solution 1
 def isPalindrome(s: str) -> bool:
     s = s.lower()
     new_str = ''.join([i for i in s if (ord(i)>=97 and ord(i)<=122) or (ord(i)>=48 and ord(i)<=57)])
@@ -14,3 +15,18 @@ def isPalindrome(s: str) -> bool:
 
 print(isPalindrome("a b c c b a"))
         
+# solution 2
+
+def isPalindrome_2(s: str) -> bool:
+    s = s.lower()
+    print(s)
+    new_str = ''.join([i for i in s if (ord(i)>=97 and ord(i)<=122) or (ord(i)>=48 and ord(i)<=57)])
+    for i in range(len(new_str)//2):
+        if new_str[i] == new_str[len(new_str)-i-1]:
+            continue
+        else:
+            return False
+    else:
+        return True
+
+print(isPalindrome_2("a b c c b a"))
