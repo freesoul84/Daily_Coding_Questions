9ec8a777a42ae06945654ee6eaab69be151dbc5c
Alkesha Baikar <baikaralkesha@gmail.com>
2024-05-25 12:24:53 +0530

    Create unique_email.py

---
+++ unique_email.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        result = set()
+        for email in emails:
+            local, domain = email.split("@")
+            new = ""
+            for i in local:
+                if i == '.':
+                    continue
+                elif i == '+':
+                    break
+                else:
+                    new += i
+            result.add(new + "@" + domain)
+
+        return len(result)
+
+
+obj = Solution()
+print(obj.numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]))
