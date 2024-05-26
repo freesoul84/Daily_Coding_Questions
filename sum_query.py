9dcaf43b3af7cf160ddc496a4a80b9c435106060
Alkesha Baikar <baikaralkesha@gmail.com>
2024-05-26 22:14:08 +0530

    Create sum_query.py

---
+++ sum_query.py
@@ -0,0 +1,18 @@
+#
+class NumArray:
+
+    def __init__(self, nums):
+        self.nums = nums
+
+    def sumRange(self, left, right) :
+        res = 0
+        for i in self.nums[left:right + 1]:
+            res += i
+        return res
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+
+a = NumArray([1,2,3,4,5,6])
+print(a.sumRange(0,3))
