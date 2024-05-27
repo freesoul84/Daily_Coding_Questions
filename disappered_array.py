f089b05b292cbcb3889843dcce60d83d84af11dc
Alkesha Baikar <baikaralkesha@gmail.com>
2024-05-27 22:33:22 +0530

    Update disappered_array.py

--- disappered_array.py
+++ disappered_array.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 class Solution:
     def findDisappearedNumbers(self, nums):
         return set(range(1,len(nums)+1)) - set(nums)
