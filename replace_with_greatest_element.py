1463733b603dd02b56e88ff73a5f667ad8097411
Alkesha Baikar <baikaralkesha@gmail.com>
2024-09-19 13:44:03 +0530

    Update replace_with_greatest_element.py

--- replace_with_greatest_element.py
+++ replace_with_greatest_element.py
@@ -9,3 +9,13 @@ class Solution:
             else:
                 arr[i] = -1
         return arr
+    
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        rightmax = -1
+        for i in range(len(arr)-1,-1,-1):
+            newmax = max(rightmax,arr[i])
+            arr[i] = rightmax
+            rightmax = newmax
+        return arr
