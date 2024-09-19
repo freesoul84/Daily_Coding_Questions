2e6aed49885a08e59815dee25f32743b28b3da6d
Alkesha Baikar <baikaralkesha@gmail.com>
2024-09-19 16:22:05 +0530

    Create length_of_last_word.py

---
+++ length_of_last_word.py
@@ -0,0 +1,11 @@
+# length of last word
+# https://leetcode.com/problems/length-of-last-word/
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i,length  = len(s)-1,0
+        while s[i]==' ':
+            i -= 1
+        while s[i] != ' ' and i >=0:
+            length += 1
+            i -= 1
+        return length
