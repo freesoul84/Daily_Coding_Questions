2075e86eb7fe47b8031feb0227b3371a7fe4430e
Alkesha Baikar <baikaralkesha@gmail.com>
2024-10-06 12:26:20 +0530

    Create walrus_operator.py

---
+++ python_new_features_learning/walrus_operator.py
@@ -0,0 +1,8 @@
+# implementation of walrus operator to see its use and working
+
+if num:=input("Enter the number :"):
+    print(f"the number is {num}")
+
+
+for i in range(start:=int(input("Start range :")),end:=int(input("End range :"))):
+    print(i,end=",")
